da2b85398cf9cd72c959d88ec5b8de842b68e04c
Mohammed Zaid <mdz4id@gmail.com>
2026-01-09 18:52:10 +0530

    Update database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,8 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: Number(process.env.DB_PORT) || 5432,
+  connectionString:process.env.DB_URL,
+ 
   
   // Connection pooling configuration
   max: 20,                    // Max connections in pool
